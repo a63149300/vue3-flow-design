056f5cb2334a4fdb45874d373935680881025c16
张业平 <a63149300@163.com>
2022-08-27 09:31:56 +0800

    perf: 修改组件列表文件名

--- src/views/modules/FlowElement.vue
+++ src/views/modules/FlowElement.vue
@@ -2,7 +2,7 @@
   <a-layout-sider width="44" class="flow-element">
     <a-row>
       <div class="tab">基础</div>
-      <element-list
+      <FlowElementList
         :nodeList="commonNodes"
         :belongTo="NodeTypeEnum.Common_Node_Type"
         @setDragInfo="setDragInfo"
@@ -10,7 +10,7 @@
     </a-row>
     <a-row>
       <div class="tab">高级</div>
-      <element-list
+      <FlowElementList
         :nodeList="highNodes"
         :belongTo="NodeTypeEnum.High_Node_Type"
         @setDragInfo="setDragInfo"
@@ -18,7 +18,7 @@
     </a-row>
     <a-row>
       <div class="tab">泳道</div>
-      <element-list
+      <FlowElementList
         :nodeList="laneNodes"
         :belongTo="NodeTypeEnum.Lane_Node_Type"
         @setDragInfo="setDragInfo"
@@ -28,7 +28,7 @@
 </template>
 
 <script lang="ts" setup>
-  import ElementList from './ElementList.vue';
+  import FlowElementList from './FlowElementList.vue';
   import { commonNodes, highNodes, laneNodes } from '/@/config/basic-node-config';
   import { IDragInfo } from '/@/type/index';
   import { NodeTypeEnum } from '/@/type/enums';
